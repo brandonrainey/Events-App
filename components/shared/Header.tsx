bd5024ae0693255112f3d0ed335e5b0898479907
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-13 23:13:07 -0400

    first commit

---
+++ components/shared/Header.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
+
+export default function Header() {
+  return (
+    <header className='w-full border-b'>
+        <div className='wrapper flex items-center justify-between'>
+            <Link href={'/'} className='w-36'>
+                <Image src={'/assets/images/logo.svg'} alt='Events App Logo' width={128} height={38} />
+            </Link>
+
+            <SignedIn>
+                <nav className='md:flex-between hidden w-full max-w-xs'>
+                    <NavItems />
+                </nav>
+                
+            </SignedIn>
+
+            <div className='flex w-32 justify-end gap-3'>
+                <SignedIn>
+                    <UserButton afterSignOutUrl={'/'} />
+                    <MobileNav />
+                </SignedIn>
+                <SignedOut>
+                    
+                        <Button asChild >
+                            <Link href={'/sign-in'}>Login</Link>
+                        </Button>
+                    
+                </SignedOut>
+            </div>
+        
+        </div>
+    </header>
+  )
+}
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NavItems() {
+  return (
+    <ul className='md:flex-between flex'>NavItems</ul>
+  )
+}
