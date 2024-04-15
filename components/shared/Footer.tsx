74128ebba4691e32ff63c770dbd0af45775a9c6a
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-14 23:18:11 -0400

    connected to mongodb, added webhooks for clerk integration

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='border-t'>
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href={'/'}>
+          <Image src={'/assets/images/logo.svg'} alt='Events App Logo' width={128} height={38} />
+        </Link>
+        <p>2024 Events App. All Rights Reserved</p>
+      </div>
+    </footer>
   )
 }
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,27 @@
+'use client'
+
 import React from 'react'
+import { headerLinks } from '@/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function NavItems() {
+  const pathname = usePathname()
+
   return (
-    <ul className='md:flex-between flex'>NavItems</ul>
+    <ul className='md:flex-between flex w-full flex-col items-start gap-5 md:flex-row'>
+      {headerLinks.map((link) => {
+
+        const isActive = pathname === link.route
+
+        return (
+          <li className={`${isActive && 'text-primary-500'} flex-center p-medium-16 whitespace-nowrap`}>
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        )
+      }
+        
+      )}
+    </ul>
   )
 }
