83a7a2e4b642688c962c2d55500292da6d041bb8
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-15 20:32:40 -0400

    fixed regex issue in clerk middleware

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+
 export default function Home() {
   return (
     <>
@@ -19,7 +20,7 @@ export default function Home() {
             </Button>
           </div>
 
-          <Image src='/assets/images/hero.png' alt='Hero Image' width={1000} height={1000} className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"/>
+          <Image src={'/assets/images/hero.png'} alt='Hero Image' width={1000} height={1000} className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"/>
 
           
 
