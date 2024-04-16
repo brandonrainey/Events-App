8b1b846bd6895fef0974e60ff3377b6fb4b5e33f
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-16 17:42:55 -0400

    clerk fix test

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -5,6 +5,8 @@ const MONGODB_URI = process.env.MONGODB_URI
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
+    
+
     if (cached.conn) return cached.conn;
     
     if (!MONGODB_URI) throw new Error('MONGODB_ULI is missing')
