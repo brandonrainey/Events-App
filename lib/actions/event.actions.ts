f5ae9019bd76f5704d0faeeb6ed9facb56959cfb
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-19 23:27:49 -0400

    added main page events display

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,14 +1,16 @@
 'use server'
 
-import { CreateEventParams } from '@/types'
+import { CreateEventParams, DeleteEventParams, GetAllEventsParams } from '@/types'
 import { handleError } from '../utils'
 import { connectToDatabase } from '../mongodb/database'
 import User from '../mongodb/database/models/user.model'
 import Event from '../mongodb/database/models/event.model'
 import Category from '../mongodb/database/models/category.model'
+import { revalidatePath } from 'next/cache'
 
 const populateEvent = async (query: any) => {
     return query
+        .populate({ path: 'organizer', model: User, select: 'username'})
         .populate({ path: 'category', model: Category, select: '_id name'})
 }
 
@@ -52,4 +54,43 @@ export const getEventById = async (eventId: string) => {
     } catch (error) {
         handleError(error)
     }
+}
+
+export const getAllEvents = async ({query, limit = 6, page, category}: GetAllEventsParams) => {
+  try {
+      await connectToDatabase()
+
+      const conditions = {
+        
+      }
+
+      const eventsQuery = Event.find(conditions)
+          .sort({ createdAt: 'desc'})
+          .skip(0)
+          .limit(limit)
+
+      const events = await populateEvent(eventsQuery)
+      const eventsCount = await Event.countDocuments(conditions)
+
+      return {
+        data: JSON.parse(JSON.stringify(events)),
+        totalPages: Math.ceil(eventsCount / limit),
+      }
+  } catch (error) {
+      handleError(error)
+  }
+}
+
+export const deleteEvent = async ({eventId, path}: DeleteEventParams) => {
+  try {
+      await connectToDatabase()
+
+      const deletedEvent = await Event.findByIdAndDelete(eventId)
+
+      if (deletedEvent) revalidatePath(path)
+
+      return JSON.parse(JSON.stringify(event))
+  } catch (error) {
+      handleError(error)
+  }
 }
--- lib/mongodb/database/models/event.model.ts
+++ lib/mongodb/database/models/event.model.ts
@@ -13,7 +13,9 @@ export interface IEvent extends Document {
     isFree: boolean;
     url?: string;
     category: {_id: string, name: string};
-    organizer: {_id: string, firstName: string, lastName: string};
+    organizer: {
+        username: ReactNode;_id: string, firstName: string, lastName: string
+};
 }
 
 const EventSchema = new Schema({
