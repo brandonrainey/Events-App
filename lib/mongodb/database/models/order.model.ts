571c0aa7fc096a877c04b7b19a7064671aec6a96
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-23 18:47:24 -0400

    reverted stripe order object metadata conditional

--- lib/mongodb/database/models/order.model.ts
+++ lib/mongodb/database/models/order.model.ts
@@ -40,10 +40,12 @@ const OrderSchema = new Schema({
   event: {
     type: Schema.Types.ObjectId,
     ref: 'Event',
+    
   },
   buyer: {
     type: Schema.Types.ObjectId,
     ref: 'User',
+    
   },
 })
 
