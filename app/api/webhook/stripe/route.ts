696fd420390886fca426012f17bb3a9ffc08a97a
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-22 21:56:34 -0400

    stripe webhook and order action reset

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -2,9 +2,7 @@ import stripe from 'stripe'
 import { NextResponse } from 'next/server'
 import { createOrder } from '@/lib/actions/order.actions'
 
-
 export async function POST(request: Request) {
-    console.log('⚠️⚠️⚠️⚠️DOES THIS EVEN WORK HELLO WHATS GOING ON!@@!#!$#$@#@⚠️⚠️⚠️⚠️⚠️')
   const body = await request.text()
 
   const sig = request.headers.get('stripe-signature') as string
@@ -15,18 +13,14 @@ export async function POST(request: Request) {
   try {
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
   } catch (err) {
-    console.log(`⚠️  Webhook signature verification failed.`, err)
     return NextResponse.json({ message: 'Webhook error', error: err })
   }
 
   // Get the ID and type
   const eventType = event.type
 
-//   console.log('event type: ', eventType)
-
   // CREATE
   if (eventType === 'checkout.session.completed') {
-    
     const { id, amount_total, metadata } = event.data.object
 
     const order = {
