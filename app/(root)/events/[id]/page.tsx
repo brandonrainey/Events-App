ecf3d33dd2cf0571d1962619bcd1f98a76f47e86
Brandon Rainey <brandonhrainey@gmail.com>
2024-04-28 18:20:01 -0400

    removed background pattern from several routes

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -20,7 +20,7 @@ export default async function EventDetails({ params: { id }, searchParams }: Sea
   return (
 
     <>
-    <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
+    <section className='flex justify-center bg-primary-50  bg-contain'>
         <div className='grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl'>
             <Image src={event.imageUrl} alt={`hero image`} width={1000} height={1000} className="max-h-full min-h-[300px] object-cover object-center"/>
 
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -24,7 +24,7 @@ export default async function ProfilePage({ searchParams }: SearchParamProps) {
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      <section className="bg-primary-50  bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
           <Button asChild className="button hidden sm:flex">
@@ -46,7 +46,7 @@ export default async function ProfilePage({ searchParams }: SearchParamProps) {
         />
       </section>
 
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+      <section className="bg-primary-50 bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
           <Button asChild className="button hidden sm:flex">
